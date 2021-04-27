65f6571dc0546e4cd15117097f2f3d3f3318caea
epernod <erik.pernod@gmail.com>
2021-04-27 23:49:46 +0200

    [SofaBaseTopology] Use directly the m)changeList from the TopologyContainer

--- SofaKernel/modules/SofaCore/src/sofa/core/topology/TopologyHandler.h
+++ SofaKernel/modules/SofaCore/src/sofa/core/topology/TopologyHandler.h
@@ -47,9 +47,6 @@ public:
     void update() override;
 
 public:
-    // really need to be a Data??
-    Data <std::list<const TopologyChange *> >m_changeList;
-
     typedef std::function<void(const core::topology::TopologyChange*)> TopoChange_callback;
 
     virtual void ApplyTopologyChanges(const std::list< const core::topology::TopologyChange*>& _topologyChangeEvents, const Size _dataSize);
