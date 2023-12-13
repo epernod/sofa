aebb9d1ac9cbad4f7729df5c69e709c4d3f21636
epernod <erik.pernod@gmail.com>
2023-12-13 17:47:21 +0100

    [QtGui] Add option to add Dockwidget from outside the main RealGUI

--- Sofa/GUI/Qt/src/sofa/gui/qt/RealGUI.h
+++ Sofa/GUI/Qt/src/sofa/gui/qt/RealGUI.h
@@ -261,6 +261,8 @@ public:
 
     void dropEvent(QDropEvent* event) override;
 
+    void addCustomDockWidget(QDockWidget* widget);
+
 protected:
     /// init data member from RealGUI for the viewer initialisation in the GUI
     void init();
